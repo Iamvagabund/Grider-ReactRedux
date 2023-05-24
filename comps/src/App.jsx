46449844e3f1c97461d0ce9fc46d2a83fc5fca48
Yuri Vynnyk <yuri.vynnyk@gmail.com>
2023-05-24 17:40:15 +0300

    start section10 vite

---
+++ comps/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <div>
+        <Button>Buy now</Button>
+      </div>
+      <div>
+        <Button>See deal</Button>
+      </div>
+      <div>
+        <Button>Hide ads</Button>
+      </div>
+      <div>
+        <Button>Something</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
